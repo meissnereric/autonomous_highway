4250a291410dbe5ae67d1b6799781031030cafcf
eric <meissner.eric.7@gmail.com>
2015-03-20 17:13:08 -0600

    Initial Commit

---
+++ road.py
@@ -0,0 +1,26 @@
+class Road:
+    def __init__(self):
+        self.lanes = Lane[]
+        self.cars = []
+class Cell:
+    def __init__(self):
+        self.start = 0
+        self.filled = false
+class Car:
+    def __init__(self):
+        self.x = 0
+        self.exit = 10
+class Lane:
+    def __init__(self, length):
+        self.cells = [length]
+        #openings could be created as a list comprehension
+        #define functional things to create openings/etc
+        self.openings = [length]
+def How():
+    return 0;
+
+def When():
+    return 0;
+
+def Do():
+    return 0;
